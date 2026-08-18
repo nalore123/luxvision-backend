1b141911e6e9adc9296cc7abf94bbc17ac1f4e1c
nalore123 <kosiclorena@gmail.com>
2026-08-18 13:11:08 +0200

    Dodana STATICFILES_STORAGE za kompatibilnost s cloudinary_storage paketom

--- config/settings/production.py
+++ config/settings/production.py
@@ -24,3 +24,5 @@ STORAGES = {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
