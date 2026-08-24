a44ded40c5dae0756cb04455177825f18bb83668
nalore123 <kosiclorena@gmail.com>
2026-08-24 18:16:09 +0200

    Dodan LOGGING config da vidimo request greske u produkciji

--- config/settings/production.py
+++ config/settings/production.py
@@ -34,3 +34,24 @@ EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 10
 DEFAULT_FROM_EMAIL = env("EMAIL_HOST_USER")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
